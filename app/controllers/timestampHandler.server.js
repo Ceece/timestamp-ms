11bfe25581eb1ac2bf0c931cbda800416a9f153a
Teerapong Manakul <imumi@msn.com>
2016-02-16 15:19:49 +0700

    add response date function

--- app/controllers/timestampHandler.server.js
+++ app/controllers/timestampHandler.server.js
@@ -6,11 +6,16 @@ function timestampHandler(req, res)  {
 	var stringDate = req.params.stringDate;
 	if ((stringDate + '').match(/\d{10}/)) {
 		var date = moment(stringDate * 1000);
+		jsonDate(res, date);
 	} else if (stringDate.match(/\w+\s\d+\,\s\d{4}/)) {
 		var date = moment(stringDate, 'MMMM D, YYYY');
+		jsonDate(res, date);
 	} else {
 		res.json(null);
 	}
+}
+
+function jsonDate(res, date) {
 	res.json({
 		"unix": date.format('x') / 1000,
 		"natural": date.format('MMMM D, YYYY')
