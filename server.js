40cd5dbbd2fc8375759f736fb85c3000310e39d9
Teerapong Manakul <imumi@msn.com>
2016-02-16 13:22:23 +0700

    change port to 8080

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.set('view engine', 'jade');
 
 routes(app);
 
-app.listen(3000, function () {
-   console.log('Node.js listening on port 3000...');
+app.listen(8080, function () {
+   console.log('Node.js listening on port 8080...');
 });
