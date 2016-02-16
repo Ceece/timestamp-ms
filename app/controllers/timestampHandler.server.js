32f0e397bedc104030e1f2d8e438b8d395d0a0e6
Teerapong Manakul <imumi@msn.com>
2016-02-16 15:06:17 +0700

    add null case

--- app/controllers/timestampHandler.server.js
+++ app/controllers/timestampHandler.server.js
@@ -4,9 +4,13 @@ var moment = require('moment').utc;
 
 function timestampHandler(req, res)  {
 	var stringDate = req.params.stringDate;
-	if ((stringDate + '').match(/\d{10}/))
-		stringDate *= 1000;
-	var date = moment(stringDate);
+	if ((stringDate + '').match(/\d{10}/)) {
+		var date = moment(stringDate * 1000);
+	} else if (stringDate.match(/\w+\s\d+\,\s\d{4}/)) {
+		var date = moment(stringDate, 'MMMM D, YYYY');
+	} else {
+		res.json(null);
+	}
 	res.json({
 		"unix": date.format('x') / 1000,
 		"natural": date.format('MMMM D, YYYY')
