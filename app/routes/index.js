03d6f24d49492c57f00157791f7abeb4eae94982
Teerapong Manakul <imumi@msn.com>
2016-02-16 10:58:45 +0700

    remove unnecessary

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,13 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
 var timestampHandler = require(process.cwd() + '/app/controllers/timestampHandler.server.js');
 
-module.exports = function (app, db) {
-    var clickHandler = new ClickHandler(db);
+module.exports = function (app) {
 
     app.route('/')
     .get(function (req, res) {
         res.sendFile(process.cwd() + '/public/index.html');
     });
-
-    app.route('/api/clicks')
-    .get(clickHandler.getClicks)
-    .post(clickHandler.addClick)
-    .delete(clickHandler.resetClicks);
 
     app.route('/:stringDate').get(timestampHandler);
 };
